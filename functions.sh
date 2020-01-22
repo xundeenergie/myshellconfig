c76952df207a48f114e45ecfa90b083ca4afa08b
Jakobus Schürz <jakobus.schuerz>
2020-01-22 10:59:17 +0100

    debug

--- functions.sh
+++ functions.sh
@@ -255,12 +255,9 @@ sshs() {
     cat << EOF >> "${TMPBASHCONFIG}"
 [ -e /etc/bashrc ] && BASHRC=/etc/bashrc
 [ -e /etc/bash.bashrc ] && BASHRC=/etc/bash.bashrc
-echo TEST1 >&2
 . \$BASHRC
-echo TEST2 >&2
 
 for i in /etc/profile.d/*.sh; do
-echo TEST3 >&2
     if [ -r "$i" ];then
         if [ "$PS1" ]; then
             . "$i"
@@ -268,7 +265,6 @@ echo TEST3 >&2
             . "$i" >/dev/null
         fi
     fi
-echo TEST3 >&2
 done
 EOF
 
