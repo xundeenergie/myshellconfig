023d0a036dc60ec5fbc3dcc3bd7414a91929609f
Jakobus Schürz <jakobus.schuerz@schuerz.at>
2020-12-05 19:01:17 +0100

    add function crtime

--- functions.sh
+++ functions.sh
@@ -886,4 +886,14 @@ EOF
 rescan_scsi () {
     echo "- - -" > /sys/class/scsi_host/host0/scan
 }
+
+get_crtime() {
+  for target in "${@}"; do
+    inode=$(stat -c %i "${target}")
+    fs=$(df  --output=source "${target}"  | tail -1)
+    crtime=$(sudo debugfs -R 'stat <'"${inode}"'>' "${fs}" 2>/dev/null | 
+    grep -oP 'crtime.*--\s*\K.*')
+    printf "%s\t%s\n" "${target}" "${crtime}"
+  done
+    }
 #EOF
