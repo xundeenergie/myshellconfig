01f532546866fa7e56ede05ba17ec80c0bf44ca3
Jakobus Schürz <jakobus.schuerz@schuerz.at>
2021-09-20 16:01:34 +0200

    change function

--- functions.sh
+++ functions.sh
@@ -858,7 +858,7 @@ token(){
 
 token-extract-pubkey() {
     if pkcs11-tool --module $P11M --list-token-slots >&2 ;then
-        ssh-keygen -i -m pkcs8 -f <(pkcs11-tool --module $P11M -r --type pubkey $1 $2 |openssl rsa -pubin -inform DER )
+        ssh-keygen -i -m pkcs8 -f <(pkcs11-tool --module $P11M -r --type pubkey ${1:---label} ${1} |openssl rsa -pubin -inform DER )
         if [ $? -gt 0 ] ; then
             token-list-objects
         fi
